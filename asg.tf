0ff720711cbfc02dd44e15ea8722376cf9cb0102
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-25 16:21:59 -0600

    asdf

--- asg.tf
+++ asg.tf
@@ -7,7 +7,7 @@ module "asg" {
 
   image_id        = "${var.image_id}"
   instance_type   = "${var.instance_type}"
-  security_groups = "${var.security_groups}"
+  security_groups = "${var.security_group}"
 
   ebs_block_device = [
     {
