063306e292018b817767a547f66b8da3a47f51ad
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-25 15:35:44 -0600

    dev created

---
+++ asg.tf
@@ -0,0 +1,48 @@
+module "asg" {
+  source = "terraform-aws-modules/autoscaling/aws"
+
+  # Launch configuration
+  lc_name = "${var.lc_name}"
+
+  image_id        = "${var.image_id}"
+  instance_type   = "${var.instance_type}"
+  security_groups = "${var.security_groups}"
+
+  ebs_block_device = [
+    {
+      device_name           = "/dev/xvdz"
+      volume_type           = "gp2"
+      volume_size           = "50"
+      delete_on_termination = true
+    },
+  ]
+
+  root_block_device = [
+    {
+      volume_size = "50"
+      volume_type = "gp2"
+    },
+  ]
+
+  # Auto scaling group
+  asg_name                  = "${var.asg_name}"
+  vpc_zone_identifier       = "${var.vpc_zone_identifier}"
+  health_check_type         = "EC2"
+  min_size                  = "${var.min_size}"
+  max_size                  = "${var.max_size}"
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "dev"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = "megasecret"
+      propagate_at_launch = true
+    },
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region"{}
+variable "lc_name" {}
+variable  "image_id" {}
+variable  "instance_type" {}
+variable  "security_group" {}
+variable  "device_name" {}
+variable "device_type" {}
+variable "volume_size" {}
+variable    "asg_name"  {}
+variable "vpc_zone_identifier" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "desired_capacity"{}
+variable "wait_for_capacity_timeout" {}
