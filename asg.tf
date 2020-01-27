0df37672c79bfb72af343398016811b16159487e
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-27 13:57:04 -0600

    edit

--- asg.tf
+++ asg.tf
@@ -7,7 +7,7 @@ module "asg" {
 
   image_id        = "${var.image_id}"
   instance_type   = "${var.instance_type}"
-  security_group_ids = ["${aws_security_group_id.allow_80_443.name}"]
+  security_groups = ["${aws_security_group.allow_80_443.name}"]
 
 
   # Auto scaling group
