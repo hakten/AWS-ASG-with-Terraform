fdc8b93b949d5d6b9c8a8ffa0282bd999b461b10
Huseyin Akten <husakten@gmail.com>
2020-02-02 11:49:57 -0600

    setenv changed

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash 
+
 echo $0
 if [ "$0" = "$BASH_SOURCE" ]
 then
@@ -14,9 +15,6 @@ then
    return 1
 fi
 
-
-
-
 # Get directory we are running from
 DIR=$(pwd)
 DATAFILE="$DIR/$1"
@@ -29,19 +27,17 @@ if [ ! -f "$DATAFILE" ]; then
     return 1
 fi
 
-
-
 # Get env from DATAFILE
-ENVIRONMENT=$(sed -nr 's/^\s*environment\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
+
+S3FOLDERREGION=$(sed -nr 's/^\s*s3_folder_region\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
 S3BUCKET=$(sed -nr 's/^\s*s3_bucket\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-S3BUCKETPROJ=$(sed -nr 's/^\s*s3_folder_project\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-S3BUCKETREGION=$(sed -nr 's/^\s*s3_folder_region\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-S3BUCKETTYPE=$(sed -nr 's/^\s*s3_folder_type\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
+S3FOLDERPROJ=$(sed -nr 's/^\s*s3_folder_project\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
+S3FOLDERTYPE=$(sed -nr 's/^\s*s3_folder_type\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
 S3TFSTATEFILE=$(sed -nr 's/^\s*s3_tfstate_file\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-BASE_DOMAIN=$(sed -nr 's/^\s*base_domain\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-if [ -z "$ENVIRONMENT" ]
+
+if [ -z "$S3FOLDERREGION" ]
 then
-   echo "setenv: 'environment' variable not set in configuration file."
+   echo "setenv: 's3_folder_region' variable not set in configuration file."
    return 1
 fi
 if [ -z "$S3BUCKET" ]
@@ -49,17 +45,12 @@ then
    echo "setenv: 's3_bucket' variable not set in configuration file."
    return 1
 fi
-if [ -z "$S3BUCKETPROJ" ]
+if [ -z "$S3FOLDERPROJ" ]
 then
   echo "setenv: 's3_folder_project' variable not set in configuration file."
   return 1
 fi
-if [ -z "$S3BUCKETREGION" ]
-then
-   echo "setenv: 's3_folder_region' variable not set in configuration file."
-   return 1
-fi
-if [ -z "$S3BUCKETTYPE" ]
+if [ -z "$S3FOLDERTYPE" ]
 then
    echo "setenv: 's3_folder_type' variable not set in configuration file."
    return 1
@@ -70,27 +61,157 @@ then
    echo "e.g. s3_tfstate_file=\"infrastructure.tfstate\""
 return 1
 fi
+
 cat << EOF > "$DIR/backend.tf"
 terraform {
 backend "s3" {
-bucket = "${S3BUCKET}"
-key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}"
-region = "${S3BUCKETREGION}"
+region  = "${S3FOLDERREGION}"
+bucket  = "${S3BUCKET}"
+key     = "${S3FOLDERPROJ}/${S3FOLDERTYPE}/${S3TFSTATEFILE}"
   }
 }
 EOF
+
+# variables to be set
+
+# s3_folder_region
+# s3_bucket
+# s3_folder_project
+# s3_folder_type
+# s3_tfstate_file
+
 cat backend.tf
 rm -rf  .terraform/terraform.tfstate
 terraform init
 
-
-
-echo "#################"
-
-
-echo "Please use terraform 12.0.0"
+########################################
 
 
 
 
-echo "#################"
+
+
+
+# echo $0 
+# if [ "$0" = "$BASH_SOURCE" ] 
+# then 
+#    echo "$0: Please source this file." 
+#    echo "e.g. source ./setenv configurations/data-rnd-us-vet1-v1" 
+#    return 1 
+# fi 
+
+# if [ -z "$1" ] 
+# then 
+#    echo "setenv: You must provide the name of the configuration file." 
+#    echo "e.g. source ./setenv configurations/data-rnd-us-vet1-v1" 
+#    return 1 
+# fi 
+
+
+# # Get directory we are running from 
+
+# DIR=$(pwd) 
+# DATAFILE="$DIR/$1" 
+# if [ ! -d "$DIR/configurations" ]; then 
+#     echo "setenv: Must be run from the root directory of the terraform project." 
+#     return 1 
+# fi 
+
+# if [ ! -f "$DATAFILE" ]; then 
+#     echo "setenv: Configuration file not found: $DATAFILE" 
+#     return 1 
+# fi
+
+
+# # Get env from DATAFILE 
+
+# S3BUCKETREGION=$(sed -nr 's/^\s*s3_bucket_region\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE") 
+
+# S3BUCKET=$(sed -nr 's/^\s*s3_bucket\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE") 
+
+# S3FOLDERPROJECT=$(sed -nr 's/^\s*s3_folder_project\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
+
+# S3FOLDERNAME=$(sed -nr 's/^\s*s3_folder_name\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE") 
+
+# S3TFSTATEFILE=$(sed -nr 's/^\s*s3_tfstate_file\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE") 
+
+
+# if [ -z "$S3BUCKETREGION" ] 
+
+# then 
+
+#    echo "setenv: 's3_bucket_region' variable not set in configuration file." 
+
+#    return 1 
+
+# fi 
+
+# if [ -z "$S3BUCKET" ] 
+
+# then 
+
+#    echo "setenv: 's3_bucket' variable not set in configuration file." 
+
+#    return 1 
+
+# fi 
+
+# if [ -z "$S3FOLDERPROJECT" ] 
+
+# then 
+
+#   echo "setenv: 's3_folder_project' variable not set in configuration file." 
+
+#   return 1 
+
+# fi 
+
+# if [ -z "$S3FOLDERNAME" ] 
+
+# then 
+
+#    echo "setenv: 's3_folder_name' variable not set in configuration file." 
+
+#    return 1 
+
+# fi 
+
+# if [ -z "$S3TFSTATEFILE" ] 
+
+# then 
+
+#    echo "setenv: 's3_tfstate_file' variable not set in configuration file." 
+
+#    echo "e.g. s3_tfstate_file=\"infrastructure.tfstate\"" 
+
+# return 1 
+
+# fi 
+
+# cat << EOF > "$DIR/backend.tf" 
+# terraform { 
+# backend "s3" { 
+# region = "${S3BUCKETREGION}" 
+# bucket = "${S3BUCKET}" 
+# key = "${S3FOLDERPROJECT}/${S3FOLDERNAME}/${S3TFSTATEFILE}" 
+#   } 
+# } 
+# EOF
+
+
+# # variables to be set
+
+# # s3_bucket_region
+# # s3_bucket
+# # s3_folder_project
+# # s3_folder_name
+# # s3_tfstate_file
+
+
+# cat backend.tf 
+# rm -rf .terraform/terraform.tfstate
+# terraform init
+
+
+########################################################
+
